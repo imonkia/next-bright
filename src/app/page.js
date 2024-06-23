45f58c0d576497a8ae8878e2c50ca82505e1e221
Monica Reta <monireta@Monicas-MacBook-Pro.local>
2024-06-23 18:03:24 -0400

    completed exercise

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import { Code } from 'bright';
 
+import Revealable from '../components/Revealable/Revealable';
+
 function Home() {
-  const [
-    isFirstSnippetShown,
-    setIsFirstSnippetShown,
-  ] = React.useState(false);
-  const [
-    isSecondSnippetShown,
-    setIsSecondSnippetShown,
-  ] = React.useState(false);
-  const [
-    isThirdSnippetShown,
-    setIsThirdSnippetShown,
-  ] = React.useState(false);
 
   return (
     <main>
@@ -28,7 +18,7 @@ function Home() {
         based on the value you assign.
       </p>
 
-      {isFirstSnippetShown ? (
+      <Revealable>
         <Code
           className="code-snippet"
           theme="dracula"
@@ -36,17 +26,7 @@ function Home() {
         >
           {FIRST_SNIPPET}
         </Code>
-      ) : (
-        <div className="reveal">
-          <button
-            onClick={() =>
-              setIsFirstSnippetShown(true)
-            }
-          >
-            Reveal Content
-          </button>
-        </div>
-      )}
+      </Revealable>
 
       <h2>
         Control Flow: Conditionals and Loops
@@ -56,26 +36,15 @@ function Home() {
         structures like if statements, for and
         while loops.
       </p>
-
-      {isSecondSnippetShown ? (
+      <Revealable>
         <Code
           className="code-snippet"
           theme="dracula"
           lang="py"
-        >
+          >
           {SECOND_SNIPPET}
         </Code>
-      ) : (
-        <div className="reveal">
-          <button
-            onClick={() =>
-              setIsSecondSnippetShown(true)
-            }
-          >
-            Reveal Content
-          </button>
-        </div>
-      )}
+      </Revealable>
 
       <h2>Functions and Basic Data Structures</h2>
 
@@ -85,26 +54,15 @@ function Home() {
         also has built-in data structures like
         lists and dictionaries.
       </p>
-
-      {isThirdSnippetShown ? (
+      <Revealable>
         <Code
           className="code-snippet"
           theme="dracula"
           lang="py"
-        >
+          >
           {THIRD_SNIPPET}
         </Code>
-      ) : (
-        <div className="reveal">
-          <button
-            onClick={() =>
-              setIsThirdSnippetShown(true)
-            }
-          >
-            Reveal Content
-          </button>
-        </div>
-      )}
+      </Revealable>
     </main>
   );
 }
